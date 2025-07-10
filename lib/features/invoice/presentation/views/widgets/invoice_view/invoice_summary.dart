48e8f3a3671563c14d0a6825719ba755115cebcf
omerbanaemh <111679865+omerbanaemh@users.noreply.github.com>
2025-07-10 16:45:44 +0300

    refactor: update InvoiceSummary layout for improved responsiveness

--- lib/features/invoice/presentation/views/widgets/invoice_view/invoice_summary.dart
+++ lib/features/invoice/presentation/views/widgets/invoice_view/invoice_summary.dart
@@ -12,50 +12,52 @@ final InvoiceCubit cubit;
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
-        SizedBox(
-          width: SizeConfig.width(context) * 0.2,
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Subtotal', style: AppStyles.styleRegular14(context).copyWith(
-                    color: Colors.black54
-                  )),
-                  Text('\$${(cubit.subtotal).toStringAsFixed(2)}',style: AppStyles.styleRegular14(context).copyWith(
-                    color: Colors.black87
-                  )),
-                ],
-              ),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Tax (8%):', style: AppStyles.styleRegular14(context).copyWith(
-                    color: Colors.black54
-                  )),
-                  Text('\$${(cubit.tax).toStringAsFixed(2)}',style: AppStyles.styleRegular14(context).copyWith(
-                    color: Colors.black87
-                  )),
-                ],
-              ),
-              SizedBox(height: 10),
-              Divider(color: Colors.black12, thickness: 1),
-              Container(
-                padding: EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(4)),
-                   color: Colors.blue[50],
-                ),
-                child: Row(
+        Flexible(
+          child: SizedBox(
+            width:  200,
+            child: Column(
+              children: [
+                Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text('Grand Total', style: AppStyles.styleSemiBold14(context)),
-                    Text('\$${(cubit.total).toStringAsFixed(2)}',style: AppStyles.styleSemiBold14(context),),
+                    Text('Subtotal', style: AppStyles.styleRegular14(context).copyWith(
+                      color: Colors.black54
+                    )),
+                    Text('\$${(cubit.subtotal).toStringAsFixed(2)}',style: AppStyles.styleRegular14(context).copyWith(
+                      color: Colors.black87
+                    )),
                   ],
                 ),
-              ),
-            ],
+          
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Tax (8%):', style: AppStyles.styleRegular14(context).copyWith(
+                      color: Colors.black54
+                    )),
+                    Text('\$${(cubit.tax).toStringAsFixed(2)}',style: AppStyles.styleRegular14(context).copyWith(
+                      color: Colors.black87
+                    )),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Divider(color: Colors.black12, thickness: 1),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(4)),
+                     color: Colors.blue[50],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Grand Total', style: AppStyles.styleSemiBold14(context)),
+                      Text('\$${(cubit.total).toStringAsFixed(2)}',style: AppStyles.styleSemiBold14(context),),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ],
