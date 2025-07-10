8d1725e15be23d57b98511644805d2f157123e28
omerbanaemh <111679865+omerbanaemh@users.noreply.github.com>
2025-07-10 23:58:42 +0300

    create print invoice pdf

--- lib/features/invoice/presentation/manger/cubit/invoice_cubit.dart
+++ lib/features/invoice/presentation/manger/cubit/invoice_cubit.dart
@@ -1,6 +1,10 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:print_ready_invoice/features/invoice/models/product_model.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:print_ready_invoice/features/invoice/presentation/views/widgets/invoice_view/pdf_widgets/pdf_widgets.dart';
+import 'package:printing/printing.dart';
 
 part 'invoice_state.dart';
 
@@ -8,11 +12,27 @@ class InvoiceCubit extends Cubit<InvoiceState> {
   InvoiceCubit() : super(InvoiceInitial());
   final List<ProductModel> newList = [];
   final List<ProductModel> products = [
-    ProductModel(prodactName: 'Select Product...',quantity: 0, unitPrice: 0),
-    ProductModel(prodactName: 'UI/UX Design Package',quantity: 0, unitPrice: 58),
-    ProductModel(prodactName: 'Cloud Hosting (Annual)',quantity: 0, unitPrice: 94),
-    ProductModel(prodactName: 'Support & Maintenance',quantity: 0, unitPrice: 55),
-    ProductModel(prodactName: 'Custom Plugin Development',quantity: 0, unitPrice: 67),
+    ProductModel(prodactName: 'Select Product...', quantity: 0, unitPrice: 0),
+    ProductModel(
+      prodactName: 'UI/UX Design Package',
+      quantity: 0,
+      unitPrice: 58,
+    ),
+    ProductModel(
+      prodactName: 'Cloud Hosting (Annual)',
+      quantity: 0,
+      unitPrice: 94,
+    ),
+    ProductModel(
+      prodactName: 'Support & Maintenance',
+      quantity: 0,
+      unitPrice: 55,
+    ),
+    ProductModel(
+      prodactName: 'Custom Plugin Development',
+      quantity: 0,
+      unitPrice: 67,
+    ),
   ];
 
   void loadInitial() {
@@ -21,50 +41,92 @@ class InvoiceCubit extends Cubit<InvoiceState> {
   }
 
   void addItem() {
-        newList.add(ProductModel(prodactName:'Select Product...',quantity: 1, unitPrice: 0));
-      emit(InvoiceLoaded(newList));
-      _updateTotals(newList);
+    newList.add(
+      ProductModel(prodactName: 'Select Product...', quantity: 1, unitPrice: 0),
+    );
+    emit(InvoiceLoaded(newList));
+    _updateTotals(newList);
   }
 
   void removeItem(int index) {
-      newList.removeAt(index);
-      emit(InvoiceLoaded(newList));
-      _updateTotals(newList);
+    newList.removeAt(index);
+    emit(InvoiceLoaded(newList));
+    _updateTotals(newList);
   }
 
-
   void duplicateItem(int index) {
-    print('Duplicating item at index: $index    ggggg= ${newList[index].prodactName} ');
-      newList.add(ProductModel(
+    print(
+      'Duplicating item at index: $index    ggggg= ${newList[index].prodactName} ',
+    );
+    newList.add(
+      ProductModel(
         prodactName: newList[index].prodactName,
         quantity: newList[index].quantity,
         unitPrice: newList[index].unitPrice,
-      ));
-      emit(InvoiceLoaded(newList));
-      _updateTotals(newList);
+      ),
+    );
+    emit(InvoiceLoaded(newList));
+    _updateTotals(newList);
   }
 
   void updateProduct(int index, ProductModel product) {
-      newList[index].prodactName = product.prodactName;
-      newList[index].unitPrice = product.unitPrice.toDouble();
-      emit(InvoiceLoaded(newList));
-      newList.forEach((element) => print('Updated product: ${element.prodactName}, Price: ${element.unitPrice}, quantity: ${element.quantity}'),);
-      _updateTotals(newList);
+    newList[index].prodactName = product.prodactName;
+    newList[index].unitPrice = product.unitPrice.toDouble();
+    emit(InvoiceLoaded(newList));
+    newList.forEach(
+      (element) => print(
+        'Updated product: ${element.prodactName}, Price: ${element.unitPrice}, quantity: ${element.quantity}',
+      ),
+    );
+    _updateTotals(newList);
   }
 
   void updateQuantity(int index, int quantity) {
-      newList[index].quantity = quantity;
-      emit(InvoiceLoaded(newList));
-      _updateTotals(newList);
+    newList[index].quantity = quantity;
+    emit(InvoiceLoaded(newList));
+    _updateTotals(newList);
   }
 
   double subtotal = 0;
   double tax = 0;
   double total = 0;
-  
+
   void _updateTotals(List<ProductModel> items) {
-    subtotal = items.fold(0, (sum, item) => sum + item.quantity * item.unitPrice);
+    subtotal = items.fold(
+      0,
+      (sum, item) => sum + item.quantity * item.unitPrice,
+    );
     tax = subtotal * 0.08;
     total = subtotal + tax;
+  }
+
+  Future<void> printInvoice(
+    List<ProductModel> invoiceItems,
+    double subtotal,
+    double tax,
+    double total,
+  ) async {
+    final pdf = pw.Document();
+
+    pdf.addPage(
+      pw.Page(
+        build: (context) {
+          return buildInvoiceBody(
+            header: buildInvoiceHeader(),
+            infoSection: buildInvoiceInfoSection(),
+            table: buildInvoiceTable(invoiceItems),
+            summary: buildInvoiceSummary(
+              subtotal: subtotal,
+              tax: tax,
+              total: total,
+            ),
+          );
+        },
+      ),
+    );
+
+    await Printing.layoutPdf(
+      onLayout: (PdfPageFormat format) async => pdf.save(),
+    );
   }
 }
--- lib/features/invoice/presentation/views/widgets/invoice_view/custom_invoice_app_bar.dart
+++ lib/features/invoice/presentation/views/widgets/invoice_view/custom_invoice_app_bar.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:print_ready_invoice/core/utils/app_styles.dart';
 import 'package:print_ready_invoice/core/utils/size_config.dart';
+import 'package:print_ready_invoice/features/invoice/presentation/manger/cubit/invoice_cubit.dart';
 import 'package:print_ready_invoice/features/invoice/presentation/views/widgets/invoice_view/custom_button.dart';
 
 class CustomInvoiceAppBar extends StatelessWidget {
-  const CustomInvoiceAppBar({
-    super.key, required this.scaffoldKey,
-  });
+  const CustomInvoiceAppBar({super.key, required this.scaffoldKey});
 
   final GlobalKey<ScaffoldState> scaffoldKey;
 
@@ -15,39 +15,69 @@ class CustomInvoiceAppBar extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Text(
-          'Onyx IX Solutions',
-          style: AppStyles.styleSemiBold26(context),
-        ),
+        Text('Onyx IX Solutions', style: AppStyles.styleSemiBold26(context)),
 
-       SizeConfig.width(context) < SizeConfig.desktop ?
-
-        IconButton(
+        SizeConfig.width(context) < SizeConfig.desktop
+            ? IconButton(
                 onPressed: () {
                   scaffoldKey.currentState!.openDrawer();
                 },
                 icon: const Icon(Icons.menu),
-              ):         Flexible(
-          child: SizedBox(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  CustomButton(backgroundColor: Colors.white, foregroundColor: Colors.black, text: 'EN', borderRight: false,),
-                  CustomButton(backgroundColor: Colors.white, foregroundColor: Colors.black, text: 'AR',borderLeft: false,),
-                  SizedBox(width: 8),
-                  CustomButton(backgroundColor: Colors.white, foregroundColor: Colors.black, icon: Icons.light_mode),
-                  SizedBox(width: 8),
-                  CustomButton(backgroundColor: Colors.white, foregroundColor: Colors.black, text: 'AI Suggest Discount', icon: Icons.lightbulb_outline),
-                  SizedBox(width: 8),
-                  CustomButton(backgroundColor: Colors.blue, foregroundColor: Colors.white, text: 'Preview Invoice', icon: Icons.print_outlined),
-                  SizedBox(width: 8),
-                ],
+              )
+            : Flexible(
+                child: SizedBox(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        CustomButton(
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black,
+                          text: 'EN',
+                          borderRight: false,
+                        ),
+                        CustomButton(
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black,
+                          text: 'AR',
+                          borderLeft: false,
+                        ),
+                        SizedBox(width: 8),
+                        CustomButton(
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black,
+                          icon: Icons.light_mode,
+                        ),
+                        SizedBox(width: 8),
+                        CustomButton(
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black,
+                          text: 'AI Suggest Discount',
+                          icon: Icons.lightbulb_outline,
+                        ),
+                        SizedBox(width: 8),
+                        CustomButton(
+                          backgroundColor: Colors.blue,
+                          foregroundColor: Colors.white,
+                          text: 'Preview Invoice',
+                          icon: Icons.print_outlined,
+                          onPressed: () {
+                            final cubit = context.read<InvoiceCubit>();
+                            final state = cubit.state as InvoiceLoaded;
+                            cubit.printInvoice(
+                              state.productItems,
+                              cubit.subtotal,
+                              cubit.tax,
+                              cubit.total,
+                            );
+                          },
+                        ),
+                        SizedBox(width: 8),
+                      ],
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ),
-        ),
-
       ],
     );
   }
