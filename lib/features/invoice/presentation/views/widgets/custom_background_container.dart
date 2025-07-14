0af50380f78dca1cdc29f01ca1996f367aac544f
omerbanaemh <111679865+omerbanaemh@users.noreply.github.com>
2025-07-15 00:17:37 +0300

    create: Custom-Background-Container Widget

---
+++ lib/features/invoice/presentation/views/widgets/custom_background_container.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:print_ready_invoice/core/utils/size_config.dart';
+
+class CustomBackgroundContainer extends StatelessWidget {
+  const CustomBackgroundContainer({
+    super.key,
+    required this.child,
+    this.padding, 
+    this.widthRatio =  0.45,    
+  });
+
+  final Widget child;
+  final double? padding;
+  final double widthRatio;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(minWidth: 500),
+        child: Container(
+          width: SizeConfig.width(context) * widthRatio,
+          padding: EdgeInsets.all(padding ?? 20),
+          decoration: ShapeDecoration(
+            color: const Color(0xFFFDFDFD),
+            shape: RoundedRectangleBorder(
+              side: BorderSide(width: 1, color: Colors.grey.shade300),
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
