c6e74a2c1e7039130cbb71628293de1ebfdaac69
omerbanaemh <111679865+omerbanaemh@users.noreply.github.com>
2025-07-14 16:48:49 +0300

    create: Product-Details-Entity

---
+++ lib/features/invoice/domin/entities/product_details_entity.dart
@@ -0,0 +1,8 @@
+class ProductDetailsEntity {
+    final String name, type, frameWork;
+    final String? description;
+  final List<String> technologies;
+
+  ProductDetailsEntity({required this.name, required this.type, required this.frameWork, required this.description, required this.technologies});
+
+}
