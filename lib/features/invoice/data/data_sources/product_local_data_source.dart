3ee2adf1e8a85ade159093e7aa9dc6e25f39d021
omerbanaemh <111679865+omerbanaemh@users.noreply.github.com>
2025-07-15 20:48:20 +0300

    add Product in local data sort

--- lib/features/invoice/data/data_sources/product_local_data_source.dart
+++ lib/features/invoice/data/data_sources/product_local_data_source.dart
@@ -2,6 +2,7 @@ import 'package:print_ready_invoice/features/invoice/data/models/product_model.d
 
 abstract class ProductLocalDataSource {
   Future<List<ProductModel>> fetchProducts();
+  Future<void> addProduct(ProductModel product);
 }
 
 class ProductLocalDataSourceImpl implements ProductLocalDataSource {
@@ -10,5 +11,11 @@ class ProductLocalDataSourceImpl implements ProductLocalDataSource {
   @override
   Future<List<ProductModel>> fetchProducts() async {
     return Future.value(products);
+  }
+  
+  @override
+  Future<void> addProduct(ProductModel product) {
+    products.add(product);
+    return Future.value();
   }
 }
