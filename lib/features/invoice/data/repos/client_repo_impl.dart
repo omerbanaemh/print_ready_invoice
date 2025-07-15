87c5bc488890f640241282ef8b822fd89b93ef1f
omerbanaemh <111679865+omerbanaemh@users.noreply.github.com>
2025-07-15 11:26:52 +0300

    update Client-Repo-Impl

--- lib/features/invoice/data/repos/client_repo_impl.dart
+++ lib/features/invoice/data/repos/client_repo_impl.dart
@@ -1,12 +1,24 @@
 import 'package:dartz/dartz.dart';
 import 'package:print_ready_invoice/core/utils/errors/failure.dart';
+import 'package:print_ready_invoice/features/invoice/data/data_sources/client_local_data_source.dart';
 import 'package:print_ready_invoice/features/invoice/domin/entities/client_entity.dart';
 import 'package:print_ready_invoice/features/invoice/domin/repos/clinet_repo.dart';
 
 class ClientRepoImpl extends ClinetRepo {
+  final ClientLocalDataSource clientLocalDataSource;
+
+  ClientRepoImpl({required this.clientLocalDataSource});
   @override
-  Future<Either<Failure, ClientEntity>> fetchClientDetails() {
-    // TODO: implement fetchClientDetails
-    throw UnimplementedError();
+  Future<Either<Failure, ClientEntity>> fetchClientDetails() async {
+    try {
+      final ClientEntity? clientDetails = clientLocalDataSource
+          .fetchClientDetails();
+      if (clientDetails == null) {
+        return left(ServerFailure('No local data found'));
+      }
+      return right(clientDetails);
+    } catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
   }
 }
