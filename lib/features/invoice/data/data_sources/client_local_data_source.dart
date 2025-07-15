9274c7cd2859dd926c2ee0f02d6cd36f7e43a6a1
omerbanaemh <111679865+omerbanaemh@users.noreply.github.com>
2025-07-15 10:30:23 +0300

    Create client_local_data_source.dart

---
+++ lib/features/invoice/data/data_sources/client_local_data_source.dart
@@ -0,0 +1,16 @@
+import 'package:print_ready_invoice/features/invoice/domin/entities/client_entity.dart';
+
+abstract class ClientLocalDataSource {
+  ClientEntity fetchClientDetails();
+}
+
+
+class ClientLocalDataSourceImpl extends ClientLocalDataSource{
+ ClientEntity client = ClientEntity(name: 'Global Corp',address: '123 Street Name, City, 12345',email: 'contact@gmail.com');
+
+  @override
+  ClientEntity fetchClientDetails() {
+    return client;
+  }
+
+}
