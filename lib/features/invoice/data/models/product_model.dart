5652d46a54caa4dce4c748ab27ba2e2702d6f808
omerbanaemh <111679865+omerbanaemh@users.noreply.github.com>
2025-07-15 16:03:12 +0300

    Create product_model.dart

---
+++ lib/features/invoice/data/models/product_model.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/foundation.dart';
+import 'package:print_ready_invoice/features/invoice/domin/entities/product_entity.dart';
+
+class ProductModel {
+  final String prodactName;
+  final int quantity;
+  final double unitPrice;
+  ProductModel({
+    required this.prodactName,
+    required this.quantity,
+    required this.unitPrice,
+  });
+
+  factory ProductModel.fromProductEntity(ProductEntity productEntity) =>
+      ProductModel(
+        prodactName: productEntity.prodactName,
+        quantity: productEntity.quantity,
+        unitPrice: productEntity.unitPrice,
+      );
+
+  ProductEntity toProductEntity() => ProductEntity(
+    prodactName: prodactName,
+    quantity: quantity,
+    unitPrice: unitPrice,
+  );
+}
